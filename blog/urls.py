0e43606d5668e58e5b8de14f2e8c185e810fb2b9
Andrey Zhartun <qeetree@gmail.com>
2020-08-05 15:11:53 +0600

    набросок блога с sqlite

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+ 
+from . import views
+from .views import BlogListView, BlogDetailView
+ 
+urlpatterns = [
+    path('', BlogListView.as_view(), name='home'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+]
---
+++ blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+ 
+from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            #post.author = request.user
+            #post.published_date = timezone.now()
+            post.save()
+            # return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'post_edit.html', {'form': form})
